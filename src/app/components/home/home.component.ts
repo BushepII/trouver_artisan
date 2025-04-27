2a4b8a1606dfd434e66b60adb2c2187d3864f522
BushepII <bushepii@gmail.com>
2025-04-27 15:28:19 +0200

    First working version of home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { InstructionService, Instruction } from '../../services/instructions.service';
 import { CommonModule } from '@angular/common';
+import { CardArtisanComponent } from '../card-artisan/card-artisan.component';
+import { ArtisanService, Artisan } from '../../services/artisans.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, CardArtisanComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private instructionService = inject(InstructionService);
+  private artisanService = inject(ArtisanService);
 
   instructions: Instruction[] = [];
+  artisans: Artisan[] = [];
+  topArtisans: Artisan[] = [];
 
   ngOnInit(): void {
     this.instructionService.getInstructions().subscribe({
       next: (data) => {
         this.instructions = data;
+      },
+      error: (err) => console.error('Error fetching instructions:', err)
+    });
+
+    this.artisanService.getArtisans().subscribe({
+      next: (data) => {
+        this.artisans = data;
+        this.topArtisans = this.artisans.filter(artisan => artisan.top === true).slice(0, 3);
       },
       error: (err) => console.error('Error fetching instructions:', err)
     });
