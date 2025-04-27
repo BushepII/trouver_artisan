ff90cdf45d81eac9aabb94d4d722b9bea5f95ab7
BushepII <bushepii@gmail.com>
2025-04-27 22:59:23 +0200

    second version for vercel that needs getPrerenderParams function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,10 +17,15 @@ import { PageArtisanComponent } from './components/page-artisan/page-artisan.com
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'search', component: SearchComponent},
-    { path: 'artisan/:id',
+    { 
+        path: 'artisan/:id',
         loadComponent: () => import('./components/page-artisan/page-artisan.component').then(m => m.PageArtisanComponent),
         data: {
-          renderMode: 'server'
+          getPrerenderParams: () => {
+            return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }, { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' },
+                    { id: '0' }, { id: '11' }, { id: '12' }, { id: '13' }, { id: '14' }, { id: '15' }, { id: '16' }, { id: '17' }
+            ];
+          }
         }
     },
     { path: 'legal-mentions', component: LegalmentionsComponent},
